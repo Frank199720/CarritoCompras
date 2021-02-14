da99c0dcec108565a48a02edeccd758b0a996fa4
alex5n <silveracuri@gmail.com>
2021-02-13 23:54:46 -0500

    algo

--- src/app/admin/principal/principal.component.ts
+++ src/app/admin/principal/principal.component.ts
@@ -11,6 +11,41 @@ export class PrincipalComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    let items = localStorage.getItem("arreglo")
+    this.datos = JSON.parse(items);
+  }
+
+  datos = [];
+
+  arreglo = [{
+    nombre: "Maggie",
+    tipo: "Perro",
+    edad: 3,
+  }, {
+    nombre: "Guayaba",
+    tipo: "Perro",
+    edad: 1,
+  }, {
+    nombre: "Snowball",
+    tipo: "Gato",
+    edad: 1,
+  },
+  ];
+
+  elemento = {
+    nombre: "Snowball",
+    tipo: "Gato",
+    edad: 1,
+  };
+
+  agregar(){
+    let array = localStorage.getItem("arreglo");
+    if(array){
+      this.arreglo = JSON.parse(array);
+    }
+    this.arreglo.push(this.elemento);
+    localStorage.setItem("arreglo",JSON.stringify(this.arreglo));
+    console.log(this.arreglo);
   }
 
 }
