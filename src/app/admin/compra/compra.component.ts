6cea140b4dd7c146a40d65db6268f74357bc90af
alex5n <silveracuri@gmail.com>
2021-02-14 18:19:28 -0500

    deja los css´s

--- src/app/admin/compra/compra.component.ts
+++ src/app/admin/compra/compra.component.ts
@@ -3,8 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-compra',
   templateUrl: './compra.component.html',
-  styles: [
-  ]
+  styles: ['./compra.component.css']
 })
 export class CompraComponent implements OnInit {
 
